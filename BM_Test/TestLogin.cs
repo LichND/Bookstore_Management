16861b847fc18195889d79e36d4b966e66b22fca
Trung Hieu <trunghieu2570@gmail.com>
2020-06-19 18:19:28 +0700

    add automation test form MainWindow and LoginWindow

--- BM_Test/TestLogin.cs
+++ BM_Test/TestLogin.cs
@@ -11,26 +11,72 @@ namespace BM_Test
     {
         private static WindowsElement header;
 
-        [ClassInitialize]
-        public static void ClassInitialize(TestContext context)
+        [TestInitialize]
+        public void TestInitialize()
         {
-            Setup(context);
+            Setup(null);
             header = session.FindElementByXPath("/Window[@AutomationId=\"_Login\"]");
 
         }
 
         [TestMethod]
-        public void TestMethod1()
+        [DataRow("admin", "admin")]
+        public void Test_Login_Valid(string object1, string object2)
         {
-            header.FindElementByClassName("TextBox").SendKeys(Keys.LeftControl + "a" + Keys.LeftControl);
-            header.FindElementByClassName("TextBox").SendKeys("admin");
-            header.FindElementByClassName("PasswordBox").SendKeys(Keys.LeftControl + "a" + Keys.LeftControl);
-            header.FindElementByClassName("PasswordBox").SendKeys("admin");
+            header.FindElementByClassName("TextBox").SendKeys(Keys.LeftControl + "a");
+            header.FindElementByClassName("TextBox").SendKeys(object1);
+            header.FindElementByClassName("PasswordBox").SendKeys(Keys.LeftControl + "a");
+            header.FindElementByClassName("PasswordBox").SendKeys(object2);
             header.FindElementByXPath("/Window[@AutomationId=\"_Login\"]/Button[@ClassName=\"Button\"][@Name=\"Login\"]").Click();
+            var result = header.FindElementByAccessibilityId("Warning").Text;
+            Assert.AreNotEqual("Sai tài khoản hoặc mật khẩu", result);
         }
 
-        [ClassCleanup]
-        public static void ClassCleanup()
+        [TestMethod]
+        [DataRow("admin", "admin1")]
+        public void Test_Login_Invalid(string object1, string object2)
+        {
+            header.FindElementByClassName("TextBox").SendKeys(Keys.LeftControl + "a");
+            header.FindElementByClassName("TextBox").SendKeys(object1);
+            header.FindElementByClassName("PasswordBox").SendKeys(Keys.LeftControl + "a");
+            header.FindElementByClassName("PasswordBox").SendKeys(object2);
+            header.FindElementByXPath("/Window[@AutomationId=\"_Login\"]/Button[@ClassName=\"Button\"][@Name=\"Login\"]").Click();
+            var result = header.FindElementByAccessibilityId("Warning").Text;
+            Assert.AreEqual("Sai tài khoản hoặc mật khẩu", result);
+        }
+
+        [TestMethod]
+        [DataRow("admin", "")]
+        public void Test_Login_No_Password(string object1, string object2)
+        {
+            header.FindElementByClassName("TextBox").SendKeys(Keys.LeftControl + "a");
+            header.FindElementByClassName("TextBox").SendKeys(Keys.Backspace);
+            header.FindElementByClassName("TextBox").SendKeys(object1);
+            header.FindElementByClassName("PasswordBox").SendKeys(Keys.LeftControl + "a");
+            header.FindElementByClassName("PasswordBox").SendKeys(Keys.Backspace);
+            header.FindElementByClassName("PasswordBox").SendKeys(object2);
+            header.FindElementByXPath("/Window[@AutomationId=\"_Login\"]/Button[@ClassName=\"Button\"][@Name=\"Login\"]").Click();
+            var result = header.FindElementByAccessibilityId("Warning").Text;
+            Assert.AreEqual("Mật khẩu không được để trống!", result);
+        }
+
+        [TestMethod]
+        [DataRow("", "admin")]
+        public void Test_Login_No_Username(string object1, string object2)
+        {
+            header.FindElementByClassName("TextBox").SendKeys(Keys.LeftControl + "a");
+            header.FindElementByClassName("TextBox").SendKeys(Keys.Backspace);
+            header.FindElementByClassName("TextBox").SendKeys(object1);
+            header.FindElementByClassName("PasswordBox").SendKeys(Keys.LeftControl + "a");
+            header.FindElementByClassName("PasswordBox").SendKeys(Keys.Backspace);
+            header.FindElementByClassName("PasswordBox").SendKeys(object2);
+            header.FindElementByXPath("/Window[@AutomationId=\"_Login\"]/Button[@ClassName=\"Button\"][@Name=\"Login\"]").Click();
+            var result = header.FindElementByAccessibilityId("Warning").Text;
+            Assert.AreEqual("Tài khoản không được để trống!", result);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
         {
             TearDown();
         }
