eeeb6e3ee6308707a34b7074de29fb4ee07d72e7
minhnguuyenduy <minhnguyenduy99@gmail.com>
2020-06-22 21:16:31 +0700

    Add test for KhachHang, NhapKho, TaiKhoan, XuatKho

---
+++ BM_Test/Test_KhachHang.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BM_Test
+{
+    [TestClass]
+    public class Test_KhachHang : BaseTestApp
+    {
+        private static WindowsElement tab_KhachHang;
+        private static WindowsElement table_KhachHang;
+        private static WindowsElement button_Them;
+        private static WindowsElement panel_KhachHang;
+        private static WindowsElement window_Them;
+
+        
+        [TestMethod]
+        [DataRow("Khách hàng A", 1, "0987654321", "Hồ Chí Minh")]
+        public void KH034_KhachHang_Add(object tenKH, object gioitinhIndex, object sdt, object diachi)
+        {
+            button_Them.Click();
+            SwitchToPopupWindow();
+            window_Them = session.FindElementByClassName("Popup");
+
+            var textboxes = window_Them.FindElementsByTagName("Edit");
+
+            textboxes[0].SendKeys((string)tenKH);
+            Chon_GioiTinh((int)gioitinhIndex);
+            Thread.Sleep(100);
+            SwitchToPopupWindow();
+
+            textboxes[2].SendKeys((string)sdt);
+            textboxes[3].SendKeys((string)diachi);
+
+            window_Them.FindElementByName("Thêm").Click();
+
+            bool isNewKhachHangExists = table_KhachHang.FindElementsByTagName("DataItem").Any(row =>
+            {
+                var cell_Ten = row.FindElementsByClassName("DataGridCell")[1];
+                return cell_Ten.Text.Equals(tenKH);
+            });
+
+            Assert.IsTrue(isNewKhachHangExists);
+        }
+
+        [TestMethod]
+        public void KH035_NhapThieuDuLieu()
+        {
+            button_Them.Click();
+            SwitchToPopupWindow();
+            window_Them = session.FindElementByClassName("Popup");
+
+            Thread.Sleep(100);
+            SwitchToPopupWindow();
+
+            window_Them.FindElementByName("Thêm").Click();
+
+            bool isNewKhachHangExists = table_KhachHang.FindElementsByTagName("DataItem").Any(row =>
+            {
+                var cell_Ten = row.FindElementsByClassName("DataGridCell")[1];
+                return cell_Ten.Text.Equals("");
+            });
+
+            Assert.IsTrue(isNewKhachHangExists);
+        }
+
+        [TestMethod]
+        [DataRow("Khách hàng A", 1, "0987654321", "Hồ Chí Minh")]
+        public void KH036_NhapLaiDuLieu(object tenKH, object gioitinhIndex, object sdt, object diachi)
+        {
+            button_Them.Click();
+            SwitchToPopupWindow();
+            window_Them = session.FindElementByClassName("Popup");
+
+            var textboxes = window_Them.FindElementsByTagName("Edit");
+
+            textboxes[0].SendKeys((string)tenKH);
+            Chon_GioiTinh((int)gioitinhIndex);
+            Thread.Sleep(100);
+            SwitchToPopupWindow();
+
+            textboxes[2].SendKeys((string)sdt);
+            textboxes[3].SendKeys((string)diachi);
+
+            window_Them.FindElementByName("Nhập lại").Click();
+
+            bool isDuLieuBiXoa =
+                String.IsNullOrEmpty(textboxes[0].Text) &&
+                String.IsNullOrEmpty(textboxes[2].Text) &&
+                String.IsNullOrEmpty(textboxes[3].Text);
+
+            Assert.IsTrue(isDuLieuBiXoa);
+
+            new Actions(session).MoveByOffset(300, 0).Click().Perform();
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        public void KH038_XoaKhachHang(object rowIndex)
+        {
+            var rows = table_KhachHang.FindElementsByClassName("DataGridRow");
+            var preCount = rows.Count;
+            var actions = new Actions(session);
+            actions.MoveToElement(rows[(int)rowIndex]).Perform();
+            actions.MoveByOffset(-300, 0).Perform();
+            actions.Click().Perform();
+
+            session.FindElementByName("Xóa").Click();
+
+            rows = table_KhachHang.FindElementsByClassName("DataGridRow");
+
+            Assert.AreEqual(preCount - 1, rows.Count);
+        }
+
+        private static void Chon_GioiTinh(int gioitinhIndex)
+        {
+            var gioitinhCombobox = window_Them.FindElementByClassName("ComboBox");
+            gioitinhCombobox.Click();
+            SwitchToPopupWindow();
+            Thread.Sleep(100);
+            var selectedGioiTinh = session.FindElementsByClassName("ListBoxItem")[gioitinhIndex];
+            selectedGioiTinh.Click();
+        }
+
+        private static void SetUpElements()
+        {
+            tab_KhachHang = menuBar.FindElementByName("Khách hàng") as WindowsElement;
+            tab_KhachHang.Click();
+            panel_KhachHang = mainAppHeader.FindElementByTagName("Pane") as WindowsElement;
+            table_KhachHang = panel_KhachHang.FindElementByClassName("DataGrid") as WindowsElement;
+            button_Them = panel_KhachHang.FindElementByXPath("/Pane[@ClassName=\"Frame\"]/Custom[@AutomationId=\"_uc\"]/Button[@AutomationId=\"PART_Toggle\"]") as WindowsElement;
+        }
+
+        [TestInitialize]
+        public void ClassInitialize()
+        {
+            Setup(null);
+            Thread.Sleep(100);
+            loginHeader = session.FindElementByXPath("/Window[@AutomationId=\"_Login\"]");
+            Thread.Sleep(100);
+            AutoLogin();
+            NavigateMainWindow();
+            Thread.Sleep(100);
+            SetUpElements();
+        }
+
+        [TestCleanup]
+        public void Clean()
+        {
+            TearDown();
+        }
+    }
+}
---
+++ BM_Test/Test_NhapKho.cs
@@ -0,0 +1,238 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace BM_Test
+{
+    [TestClass]
+    public class Test_NhapKho : BaseTestApp
+    {
+        private static IWebElement tab_KhoSach;
+        private static IWebElement tab_NhapKho;
+        private static IWebElement button_Them;
+        private static IWebElement panel_KhoSach;
+        private static WindowsElement window_Them;
+
+        [TestMethod]
+        public void TTNK025_Nhan_ButtonThem()
+        {
+            button_Them.Click();
+            Thread.Sleep(100);
+            try
+            {
+                SwitchToPopupWindow();
+                window_Them = session.FindElementByXPath("/Window[@ClassName=\"Window\"][@AutomationId=\"_ThongTinChiTiet_XK\"]");
+                CloseWindow_NhapKho();
+            }
+            catch
+            {
+                Assert.Fail("Cửa sổ thêm không hiện");
+            }
+            Assert.IsTrue(true);
+        }
+
+
+        [TestMethod]
+        [DataRow("Nhà cung cấp A", "Ghi chú", 0, 2)]
+        public void TTNK048_NhapKho(object nccName, object ghichu, object sachIndex, object quantity)
+        {
+            button_Them.Click();
+            Thread.Sleep(100);
+            SwitchToPopupWindow();
+            window_Them = session.FindElementByXPath("/Window[@ClassName=\"Window\"][@AutomationId=\"_ThongTinChiTiet_XK\"]");
+            var textboxes = window_Them.FindElementsByClassName("TextBox");
+            textboxes[0].SendKeys(nccName.ToString());
+            textboxes[1].SendKeys(ghichu.ToString());
+
+            OpenWindow_ThemSach();
+
+            Combobox_ChonSach((int)sachIndex);
+
+            NhapSoLuongSach((int)quantity);
+
+            session.FindElementByName("Thêm").Click();
+
+            SwitchToPopupWindow();
+
+            session.FindElementByName("Đồng ý").Click();
+            SwitchToPopupWindow();
+        }
+
+        [TestMethod]
+        [DataRow("Nhà cung cấp A", "Ghi chú", 0, 2)]
+        public void TTXK049_NhapKho_KhongChonSach(object nccName, object ghichu, object sachIndex, object quantity)
+        {
+            button_Them.Click();
+            Thread.Sleep(100);
+            SwitchToPopupWindow();
+
+            window_Them = session.FindElementByXPath("/Window[@ClassName=\"Window\"][@AutomationId=\"_ThongTinChiTiet_XK\"]");
+            var textboxes = window_Them.FindElementsByClassName("TextBox");
+            textboxes[0].SendKeys(nccName.ToString());
+            textboxes[1].SendKeys(ghichu.ToString());
+
+            OpenWindow_ThemSach();
+            Thread.Sleep(100);
+
+            NhapSoLuongSach((int)quantity);
+
+            bool button_Them_Disabled = !session.FindElementByName("Thêm").Enabled;
+
+            Assert.IsTrue(button_Them_Disabled);
+
+            CloseWindow_ThemSach();
+            CloseWindow_NhapKho();
+        }
+
+        [TestMethod]
+        [DataRow("Nhà cung cấp A", "Ghi chú", 0, 2)]
+        public void TTNK050_NhapKho_NhapLai(object nccName, object ghichu, object sachIndex, object quantity)
+        {
+            button_Them.Click();
+            SwitchToPopupWindow();
+
+            window_Them = session.FindElementByXPath("/Window[@ClassName=\"Window\"][@AutomationId=\"_ThongTinChiTiet_XK\"]");
+
+            OpenWindow_ThemSach();
+
+            NhapSoLuongSach((int)quantity);
+
+            session.FindElementByName("Nhập lại").Click();
+
+            bool isDuLieuDaXoa =
+                String.Equals(session.FindElementsByTagName("Edit")[1].Text, "0") &&
+                !session.FindElementByName("Thêm").Enabled;
+
+            Assert.IsTrue(isDuLieuDaXoa);
+            
+            CloseWindow_ThemSach();
+            CloseWindow_NhapKho();
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        public void TTNK051_NhapKho_Xoa(object index)
+        {
+            var table = session.FindElementByClassName("DataGrid");
+            var rows = table.FindElementsByClassName("DataGridRow");
+
+            int beforeDeleteCount = rows.Count;
+
+            var action = new Actions(session);
+            var selectedRow = rows[(int)index];
+            action.MoveToElement(selectedRow).Perform();
+            action.MoveByOffset(-300, 0).Perform();
+            action.ContextClick().Perform();
+
+            SwitchToPopupWindow();
+            Thread.Sleep(100);
+
+            var contextMenu = session.FindElementByClassName("ContextMenu");
+            contextMenu.FindElementByClassName("MenuItem").Click();
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        [DataRow(1, 1)]
+        public void TTNK053_NhapKho_Huy(object sachIndex, object quantity)
+        {
+            var table = session.FindElementByClassName("DataGrid");
+            var rows = table.FindElementsByClassName("DataGridRow");
+            int preCount = rows.Count;
+            button_Them.Click();
+            Thread.Sleep(100);
+            SwitchToPopupWindow();
+            window_Them = session.FindElementByXPath("/Window[@ClassName=\"Window\"][@AutomationId=\"_ThongTinChiTiet_XK\"]");
+
+            OpenWindow_ThemSach();
+
+            Combobox_ChonSach((int)sachIndex);
+            Thread.Sleep(100);
+
+            NhapSoLuongSach((int)quantity);
+            Thread.Sleep(100);
+
+            session.FindElementByName("Thêm").Click();
+            Thread.Sleep(100);
+
+            CloseWindow_ThemSach();
+            CloseWindow_NhapKho();
+
+            Thread.Sleep(100);
+
+            rows = table.FindElementsByClassName("DataGridRow");
+
+            Assert.AreEqual(preCount, rows.Count);
+        }
+
+        [TestCleanup]
+        public void ClassCleanup()
+        {
+            TearDown();
+        }
+
+        [TestInitialize]
+        public void ClassInitialize()
+        {
+            Setup(null);
+            Thread.Sleep(500);
+            loginHeader = session.FindElementByXPath("/Window[@AutomationId=\"_Login\"]");
+            Thread.Sleep(100);
+            AutoLogin();
+            NavigateMainWindow();
+            Thread.Sleep(100);
+            SetUpElements();
+        }
+
+        private static void OpenWindow_ThemSach()
+        {
+            session.FindElementByXPath("/Window[@AutomationId=\"_ThongTinChiTiet_XK\"]/Custom[@AutomationId=\"_uc\"]/Button[@AutomationId=\"PART_Toggle\"]").Click();
+            SwitchToPopupWindow();
+        }
+
+        private static void CloseWindow_ThemSach()
+        {
+            var action = new Actions(session);
+            action.MoveByOffset(300, 0).Click().Perform();
+        }
+
+        private static void CloseWindow_NhapKho()
+        {
+            window_Them.FindElementByName("Hủy").Click();
+            SwitchToPopupWindow();
+        }
+
+        private static void Combobox_ChonSach(int index)
+        {
+            var combobox = session.FindElementByClassName("ComboBox");
+            combobox.Click();
+            Thread.Sleep(100);
+            var selectedSach = session.FindElementsByClassName("ListBoxItem")[index];
+            Thread.Sleep(100);
+            selectedSach.Click();
+        }
+
+        private static void NhapSoLuongSach(int quantity)
+        {
+            var textbox_Quantity = session.FindElementsByTagName("Edit")[1];
+            textbox_Quantity.Click();
+            textbox_Quantity.SendKeys(Keys.Backspace);
+            textbox_Quantity.SendKeys(quantity.ToString());
+        }
+
+        private static void SetUpElements()
+        {
+            tab_KhoSach = menuBar.FindElementByName("Kho sách");
+            tab_KhoSach.Click();
+            panel_KhoSach = mainAppHeader.FindElementsByTagName("Pane")[0];
+            tab_NhapKho = panel_KhoSach.FindElement(By.Name("Nhập kho"));
+            tab_NhapKho.Click();
+            button_Them = panel_KhoSach.FindElements(By.ClassName("Button"))[3];
+        }
+    }
+}
